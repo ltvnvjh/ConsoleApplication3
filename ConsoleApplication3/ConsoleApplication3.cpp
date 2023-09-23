23809ca455a5d36df87523d4a10c69cfb230ebdc
Яна Рибченко <yana.r23@fpm.dnu.edu.ua>
2023-09-23 22:25:32 +0300

    Add project files.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,46 @@
+// ConsoleApplication3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //program1
+    std::cout << "HELLO!!!\n";
+
+    std::cout << endl << endl << endl;
+    //program2
+    std::cout << "*****************\n";
+    std::cout << "*               *\n";
+    std::cout << "*               *\n";
+    std::cout << "*               *\n";
+    std::cout << "*               *\n";
+    std::cout << "*               *\n";
+    std::cout << "*               *\n";
+    std::cout << "*               *\n";
+    std::cout << "*****************\n";
+    std::cout << endl << endl << endl;
+    //program3
+    std::cout << "       Surname : Rybchenko\n";
+    std::cout << "          Name : Yana\n";
+    std::cout << "    Patronymic : Ihorivna\n";
+    std::cout << "        Gander : female\n";
+    std::cout << " Date of birth : 09.11.2005\n";
+    std::cout << "      Adrdress : Bohdana Khmelnytskoho, 14, Dnipro\n";
+    std::cout << "         Phone : +380 97 733 12 20\n";
+    std::cout << "Place of study : FPM, DNU named after Olesya Honchara\n";
+    std::cout << "         Group : PA-23-1\n";
+    std::cout << "       Hobbies : programming\n";
+   std:: cin.get();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
